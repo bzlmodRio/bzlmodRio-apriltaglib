b2bc39013d2cb3fbb894c5dbf247c277556effdc
PJ Reiniger <pj.reiniger@gmail.com>
2023-06-22 00:33:29 -0400

    Include version number in auto-update actions

---
+++ generate/get_version.py
@@ -0,0 +1,11 @@
+
+from get_apriltaglib_dependencies import get_apriltaglib_dependencies
+
+def main():
+    group = get_apriltaglib_dependencies()
+    print(group.version)
+
+
+if __name__ == "__main__":
+    main()
+    
