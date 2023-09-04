c67d1a1f05b9db9f4a0505574eea6c6fef0b98fd
PJ Reiniger <pj.reiniger@gmail.com>
2023-09-03 20:15:11 -0400

    Rerun autogeneration (#16)

--- tests/cpp/apriltaglib/test.cpp
+++ tests/cpp/apriltaglib/test.cpp
@@ -1,4 +1,13 @@
 
+
+#ifdef _WIN32
+#pragma warning(disable : 4200)
+#elif defined(__clang__)
+#pragma clang diagnostic ignored "-Wc99-extensions"
+#elif defined(__GNUC__)
+#pragma GCC diagnostic ignored "-Wpedantic"
+#endif
+
 #include <apriltag.h>
 
 #include "gtest/gtest.h"
