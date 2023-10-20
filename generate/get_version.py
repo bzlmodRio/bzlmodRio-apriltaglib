9aa6391cbaaeb00dc0d65a28b78a2e1248fc1a29
PJ Reiniger <pj.reiniger@gmail.com>
2023-10-20 13:51:52 -0400

    Update toolchain version

--- generate/get_version.py
+++ generate/get_version.py
@@ -3,7 +3,7 @@ from get_apriltaglib_dependencies import get_apriltaglib_dependencies
 
 def main():
     group = get_apriltaglib_dependencies()
-    print(group.version)
+    print(group.year + "." + group.version)
 
 
 if __name__ == "__main__":
